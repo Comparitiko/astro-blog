04d3799c05629d2bb27cf1579954536c9dfed0dd
Comparitiko <gabigcy@gmail.com>
2025-02-13 20:47:12 +0100

    Quitados console.logs

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,15 +2,16 @@ import { defineCollection, z } from "astro:content";
 
 const blogCollection = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.string(),
-    date: z.date(),
-    description: z.string(),
-    image: z.string(),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.date(),
+      description: z.string(),
+      image: image(),
 
-    author: z.string(),
-    tags: z.array(z.string()),
-  }),
+      author: z.string(),
+      tags: z.array(z.string()),
+    }),
 });
 
 export const collections = {
