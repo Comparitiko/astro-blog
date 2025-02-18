3fd3725de532a5964f2c54a2749e902204cfd529
Comparitiko <gabigcy@gmail.com>
2025-02-18 19:30:46 +0100

    Seccion 7 terminada

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,19 +1,36 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 const blogCollection = defineCollection({
-  type: "content",
-  schema: ({ image }) =>
-    z.object({
-      title: z.string(),
-      date: z.date(),
-      description: z.string(),
-      image: image(),
+	type: "content",
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			date: z.date(),
+			description: z.string(),
+			image: image(),
 
-      author: z.string(),
-      tags: z.array(z.string()),
-    }),
+			author: reference("author"),
+			tags: z.array(z.string()),
+
+			isDraft: z.boolean().default(false),
+		}),
+});
+
+const authorCollection = defineCollection({
+	type: "data",
+	schema: ({ image }) =>
+		z.object({
+			name: z.string(),
+			avatar: image(),
+			twitter: z.string(),
+			linkedIn: z.string(),
+			github: z.string(),
+			bio: z.string(),
+			subtitle: z.string(),
+		}),
 });
 
 export const collections = {
-  blog: blogCollection,
+	blog: blogCollection,
+	author: authorCollection,
 };
